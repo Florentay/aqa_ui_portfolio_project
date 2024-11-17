7b2ca4c640e11cc69f290e61062f7343645a5c54
Olga Gepner <gepner.olle@gmail.com>
2024-11-17 18:30:53 +0300

    update elements tests added class: TestButtonsPage added tests: - test_different_cluck_on_the_buttons

--- locators/elements_page_locators.py
+++ locators/elements_page_locators.py
@@ -54,5 +54,17 @@ class WebTablePageLocators:
     NO_ROWS_FOUND = (By.CSS_SELECTOR, 'div[class="rt-noData"]')
     COUNT_ROW_LIST = (By.CSS_SELECTOR, 'select[aria-label="rows per page"]')
 
-    #update
+    # update
     UPDATE_BUTTON = (By.CSS_SELECTOR, 'span[title="Edit"]')
+
+
+class ButtonsPageLocators:
+    DOUBLE_BUTTON = (By.CSS_SELECTOR, "button[id='doubleClickBtn']")
+    RIGHT_CLICK_BUTTON = (By.CSS_SELECTOR, "button[id='rightClickBtn']")
+    # CLICK_ME_BUTTON = (By.XPATH, '//div[3]/button')
+    CLICK_ME_BUTTON = (By.XPATH, '//button[text()="Click Me"]')
+
+    # result
+    SUCCESS_DOUBLE = (By.CSS_SELECTOR, "p[id='doubleClickMessage']")
+    SUCCESS_RIGHT = (By.CSS_SELECTOR, "p[id='rightClickMessage']")
+    SUCCESS_CLICK_ME = (By.CSS_SELECTOR, "p[id='dynamicClickMessage']")
--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,6 +1,6 @@
 import random
 import time
-from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage
+from pages.elements_page import TextBoxPage, CheckBoxPage, RadioButtonPage, WebTablePage, ButtonsPage
 
 
 class TestTextBox:
@@ -93,3 +93,20 @@ class TestWebTable:
         count = web_table_page.select_up_to_some_rows()
         assert count == [5, 10, 20, 25, 50,
                          100], "The number of rows in the table has not been changed or has changed incorrectly"
+
+
+class TestButtonsPage:
+    # можно сделать на 3 теста (надо сделать функцию по разделению теста на 3)
+    def test_different_cluck_on_the_buttons(self, driver):
+        button_page = ButtonsPage(driver, "https://demoqa.com/buttons")
+        button_page.open()
+        # time.sleep(5)
+        double = button_page.click_on_different_button("double")
+        right = button_page.click_on_different_button("right")
+        click = button_page.click_on_different_button("click")
+        # print(double)
+        # print(right)
+        # print(click)
+        assert double == "You have done a double click", "The double click button was not pressed"
+        assert right == "You have done a right click", "The right click button was not pressed"
+        assert click == "You have done a dynamic click", "The dynamic click button was not pressed"
